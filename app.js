c65212e53d26c571b8649b4895ea7cfc2df09996
TayyabKhan1221 <Tayyabkhano603@gmail.com>
2024-03-02 13:59:38 +0500

    learinig

--- app.js
+++ app.js
@@ -1,9 +1,88 @@
-alert("Tayyab Khan");
-let myName ="Tayyab Khan";
-console.log("myName");
-console.log(43 % 56);
-// let age = 75;
-let age = 50;
-console.log(50 + 2)
- 
+// alert("Tayyab Khan");
+// let myName = "Tayyab Khan";
+// console.log("myName");
+// console.log(43 % 56);
+// // let age = 75;
+// let age = 50;
+// console.log(50 + 2);
 
+
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// // alert("Welcome bro")
+// console.log("Hello\nWorld");
+
+// let a = "56";
+
+// converting string into number
+
+// a = Number(a); // 1
+// // a = parseInt(a); // 2
+// a = parseFloat(a); // 3
+// a = +a; // 4
+
+// // let b = 4;
+
+// b = String(b);
+
+// console.log(a);
+// console.log(typeof a);
+
+// if (true) {
+//     // ye code chale ga
+//     console.log("Shehzad");
+//     // console.log("Shehzad");
+//   }
+  
+  
+// if (true) {
+//     // ye code KABHI BHI NHI chale ga
+//     console.log("Shehzad");
+
+//   }
+
+// let x = 99;
+// let y = typeof x;
+// console.log(typeof y);
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a > b", a > b);
+// a += 4;
+// console.log("a =", a);
+
+// let mode = "dark";
+
+// if(mode === "dark") {
+//     color = "black";
+// } else {
+//     color = "white";
+// }
+
+// if(mode === "light") {
+ 
+// }
+
+//  console.log(color);
+
+
+// if (age > 18) {
+//     console.log("you can vote");
+// }
+
+// if (age < 18) {
+//     console.log("you cannot vote");
+// }
+
+let a = 5;
+let b = 10;
+a = a - b;
+b = a + b;
+a = b - a;
+console.log(a = b);
